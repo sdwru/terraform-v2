12c0e5adfe6c184e670e3477cb38060cd1e7e290
sdwru <40401953+sdwru@users.noreply.github.com>
2020-05-04 15:32:06 -0700

    Create OauthClient.php

---
+++ src/Entity/OauthClient.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the TerraformV2 library.
+ *
+ * (c) Sdwru https://github.com/sdwru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace TerraformV2\Entity;
+
+/**
+ *
+ *
+ */
+final class OauthClient extends AbstractEntity
+{
+    /**
+     * @var int
+     */
+    public $id;
+
+    /**
+     * @var string
+     */
+    public $type;
+
+    /**
+     * @var object
+     */
+    public $attributes;
+
+    /**
+     * @var object
+     */
+    public $relationships;
+
+    /**
+     * @var object
+     */
+    public $sshKey;
+    
+    /**
+     * @var object
+     */
+    public $links;
+}
